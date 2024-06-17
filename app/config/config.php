7505c7df1768cfa741a61e9d7cca52998f9b8b88
ValverdeSanchezFelixMarlon <valverdefelixmarlon@gmail.com>
2024-06-17 01:32:03 -0600

    Delete app directory

--- app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'database' => [
-        'host' => 'localhost',   // Cambia esto por tu host
-        'dbname' => 'id22252201_salon',    // Cambia esto por tu nombre de base de datos
-        'username' => 'id22252201_krypton',        // Cambia esto por tu nombre de usuario
-        'password' => 'Foca3232@'      // Cambia esto por tu contraseña
-    ]
-];
--- app/pages/index.php
+++
@@ -1,5 +0,0 @@
-<?php include('../templates/cabecera.php');?>
-
-Contenido de la pagina
-
-<?php include('../templates/pie.php');?>
